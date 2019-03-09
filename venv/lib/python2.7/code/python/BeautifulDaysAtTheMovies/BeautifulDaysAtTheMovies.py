cc8e6e41eabb64a341a0a9a48af7de92bcc6eb52
duynhatldn <nhatnhat393@gmail.com>
2019-03-09 21:35:29 +0700

    BeautifulDaysAtTheMovies

---
+++ venv/lib/python2.7/code/python/BeautifulDaysAtTheMovies/BeautifulDaysAtTheMovies.py
@@ -0,0 +1,9 @@
+import sys
+
+i, j, k = [int(x) for x in raw_input().strip().split(' ')]
+
+count = 0
+for v in range(i, j+1):
+    if abs(v - int(str(v)[::-1])) % k == 0:
+        count += 1
+print count
