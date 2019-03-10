2ff458afb22823036052afa36809e20885fd14ae
duynhatldn <nhatnhat393@gmail.com>
2019-03-10 23:01:38 +0700

    FairRations

---
+++ venv/lib/python2.7/code/python/FairRations/FairRations.py
@@ -0,0 +1,17 @@
+import sys
+
+
+N = int(raw_input().strip())
+B = map(int,raw_input().strip().split(' '))
+
+total_loaves = 0
+for i in range(len(B)-1):
+    if B[i]%2!=0:
+        B[i] += 1
+        B[i+1] += 1
+        total_loaves +=2
+
+if B[-1]%2 != 0:
+    print "NO"
+else:
+    print total_loaves
